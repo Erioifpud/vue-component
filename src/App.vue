87c890bb6b1f1e033ca288e39dba0076f5d2ed12
Erioifpud <erioifpud@zuo.si>
2019-01-09 00:54:06 +0800

    CodeMirror全屏

--- src/App.vue
+++ src/App.vue
@@ -11,8 +11,9 @@ export default {
 </script>
 
 <style lang="scss">
-body {
+html, body {
   margin: 0;
+  height: 100%;
 }
 
 #app {
@@ -21,6 +22,8 @@ body {
   -moz-osx-font-smoothing: grayscale;
   // text-align: center;
   color: #2c3e50;
+  height: 100%;
+  overflow: auto;
 }
 #nav {
   padding: 30px;
--- src/Sandbox.vue
+++ src/Sandbox.vue
@@ -14,5 +14,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+.sandbox {
+  height: 100%;
+}
 </style>
